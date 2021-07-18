45d35b45e08fea840aefc26ae6257d55dd4a14bc
Rohan Ahmad <rohan@hitiq.co>
2021-07-18 21:59:18 +1000

    funky drawing

--- public/ep5/sketch.js
+++ public/ep5/sketch.js
@@ -1,6 +1,10 @@
 let boxSize;
-let headerSize = 0
+let headerSize = 0;
 let launchpad;
+let ball;
+let ball_size = 10;
+let path = [];
+let xoff = 0.0;
 let keys = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,75 +16,18 @@ let keys = [
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(50);
-  
-  if ((windowHeight - headerSize) > windowWidth) {
-    boxSize = (windowWidth ) / 8;
+
+  if (windowHeight - headerSize > windowWidth) {
+    boxSize = windowWidth / 8;
   } else {
     boxSize = (windowHeight - headerSize) / 8;
   }
 
   navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-}
-
-function drawGrid() {
-
-  clear();
-  fill(0)
-
-  for (let j = 0; j < 8; j++) {
-    for (let i = 0; i < 8; i++) {
-      if (keys[j][i]) {
-        fill(0, 255, 0);
-      } else {
-        fill(255);
-      }
-
-      rect(i * boxSize, (j * boxSize) + headerSize, boxSize, boxSize);
-    }
-  }
-}  
-
-function padPressed(pad){
-  
-  let row = pad.row -1;
-  let col = pad.column - 1
-  // console.log(row, col)
-  if (keys[row][col]){
-    keys[row][col] = 0
-    launchpad.ledOff(pad) 
-  }else{
-    keys[row][col] = 1
-    launchpad.ledOn(pad, new Color(3, 0))  
-  }
- 
-}
-
-function draw() {
-  drawGrid();
-  // moveBalls()
-  
-}
-
-function mouseClicked() {
-  let col = Math.floor(mouseX / boxSize);
-  let row = Math.floor(mouseY / boxSize);
-  if (col >= 0 && col < 8 && row >= 0 && row < 8) {
-    console.log(col, row);
-    let pad = new Pad(row + 1, col + 1);
-    if(keys[row][col]){
-      keys[row][col] = 0
-      launchpad.ledOff(pad)
-    }else{
-      keys[row][col] = 1
-      launchpad.ledOn(pad, new Color(3, 0)) 
-    }
-
-  }
-
+  ball = createVector(random(boxSize * 8), random(boxSize * 8));
 }
 
 function onMIDISuccess(midiAccess) {
@@ -89,11 +36,102 @@ function onMIDISuccess(midiAccess) {
   // Clear initial launchpad state
   launchpad.clear();
 
-  launchpad.onPadPress(pad => padPressed(pad));
+  launchpad.onPadPress((pad) => padPressed(pad));
 
-  launchpad.onControlPadPress(pad => console.log(pad));
+  launchpad.onControlPadPress((pad) => console.log(pad));
 }
 
 function onMIDIFailure(msg) {
-  console.log('Failed to get MIDI access - ' + msg);
+  console.log("Failed to get MIDI access - " + msg);
+}
+
+function drawGrid() {
+  clear();
+  fill(0);
+
+  for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 8; i++) {
+      if (keys[j][i]) {
+        fill(0, 255, 0);
+        rect(i * boxSize, j * boxSize + headerSize, boxSize, boxSize);
+      } else {
+        fill(255);
+      }
+
+      
+    }
+  }
+}
+
+function padPressed(pad) {
+  let row = pad.row - 1;
+  let col = pad.column - 1;
+  if (col > 7){
+    clear();
+    launchpad.clear();
+    keys = [
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    return
+  }
+  ball.x = (col * boxSize) +  boxSize / 2 ;
+  ball.y = row * boxSize +  boxSize / 2 ;
+  path.push({ x: ball.x, y: ball.y });
+  // console.log(row, col)
+  if (keys[row][col]) {
+    keys[row][col] = 0;
+    launchpad.ledOff(pad);
+  } else {
+    keys[row][col] = 1;
+    launchpad.ledOn(pad, new Color(3, 0));
+  }
+  // moveBall();
+}
+
+function drawBall() {
+  for (let i = 1; i < path.length; i++) {
+    let b = path[i];
+    let c = path[i-1]
+    line(b.x, b.y, c.x, c.y);
+  }
+  ellipse(ball.x, ball.y, ball_size);
+}
+
+function moveBall() {
+  xoff = xoff + 0.1;
+  ball.x = noise(xoff) * boxSize * 8;
+  ball.y = noise(xoff + 1000) * boxSize * 8;
+  path.push({ x: ball.x, y: ball.y });
+}
+
+function draw() {
+  moveBall();
+  drawGrid();
+  drawBall();
+}
+
+function mouseClicked() {
+  let col = Math.floor(mouseX / boxSize);
+  let row = Math.floor(mouseY / boxSize);
+  ball.x = (col * boxSize) +  boxSize / 2 ;
+  ball.y = row * boxSize +  boxSize / 2 ;
+  path.push({ x: ball.x, y: ball.y });
+  if (col >= 0 && col < 8 && row >= 0 && row < 8) {
+    //console.log(col, row);
+    let pad = new Pad(row + 1, col + 1);
+    if (keys[row][col]) {
+      keys[row][col] = 0;
+      launchpad.ledOff(pad);
+    } else {
+      // keys[row][col] = 1;
+      launchpad.ledOn(pad, new Color(3, 0));
+    }
+  }
 }
